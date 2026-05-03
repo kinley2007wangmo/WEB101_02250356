4125a343a9c799be156121a188ac2abc19921206
kinley2007wangmo <kw8384457@gmail.com>
2026-05-03 22:41:42 +0600

    Practical 4

--- TikTok_Frontend/src/app/page.jsx
+++ TikTok_Frontend/src/app/page.jsx
@@ -1,11 +1,15 @@
-import VideoFeed from "../components/ui/VideoFeed";
-import LiveSection from "../components/ui/LiveSection";
+"use client";
+import { useEffect, useState } from "react";
+import { getVideos } from "../services/videoService";
 
 export default function Home() {
   return (
     <div>
-      <LiveSection />
-      <VideoFeed />
+      <h1>Video Feed</h1>
+
+      <video width="300" controls autoPlay loop>
+        <source src="https://www.w3schools.com/html/mov_bbb.mp4" type="video/mp4" />
+      </video>
     </div>
   );
 }
--- TikTok_Frontend/src/lib/api-config.js
+++ TikTok_Frontend/src/lib/api-config.js
@@ -1,16 +1,37 @@
 import axios from "axios";
 
-const API = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
+const API_Base_URL = 'http://localhost:8000/api';
+
+const api = axios.create({
+  baseURL: API_Base_URL,
+  header: {
+    'Content-Type': 'application/json',
+  },
 });
 
-// Attach token automatically
-API.interceptors.request.use((req) => {
-  const token = localStorage.getItem("token");
-  if (token) {
-    req.headers.Authorization = `Bearer ${token}`;
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
   }
-  return req;
-});
+);
 
-export default API;
+api.interceptors.request.use(
+  (response) => response, 
+  (error) => {
+
+    if (error.response && error.response.status === 401) {
+      localStorage.removeItem('token');
+      window.location.herf = '/login';
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default api;
